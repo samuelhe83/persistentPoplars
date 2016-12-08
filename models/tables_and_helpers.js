1158198c142adc9d19fe5571e68055c430ad41fa
Hack Reactor Students <accounts@hackreactor.com>
2016-12-07 19:19:50 -0800

    Added Company, Users, Proposal tables.

---
+++ models/tables_and_helpers.js
@@ -0,0 +1,8 @@
+//a model is a table in sequelize
+/*
+var Sequelize = require('sequelize');
+var sequelize = new('persistentPoplar', 'root', '', {
+  dialect: 'mysql',
+  host: 
+});
+*/
---
+++ server/tables_and_helpers.js
@@ -0,0 +1,26 @@
+//a model is a table in sequelize
+//sequelize automatically creates id field as primary key
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('persistentPoplar'); 
+
+//Company table
+var Company = sequelize.define('company', {})
+
+//Users table
+var Users = sequelize.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+})
+
+Users.belongsTo(Company)
+
+
+var Proposal = sequelize.define('proposal', {
+  description: Sequelize.STRING,
+  stage: Sequelize.INTEGER //make it so it can only be 0-3
+}, {
+  timestamps: true
+})
+
+Proposal.belongsTo(Company);
+Proposal.belongsTo(Users); //rename it so this category is called admin user
