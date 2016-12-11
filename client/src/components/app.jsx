0e525d4b04f7ec53e186b5a5dbd262578bcce17a
Nate Graf <njgraf512@gmail.com>
2016-12-11 03:50:18 -0800

    Update login to stateful component

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -13,12 +13,9 @@ class App extends React.Component {
     // Binding 'this' here is necessary to have 'this' in requireAuth method bound
     // to the react class instance with ES6.
     this.requireAuth = this.requireAuth.bind(this);
-    this.changeEmail = this.changeEmail.bind(this);
-    this.changePassword = this.changePassword.bind(this);
     this.loginAjax = this.loginAjax.bind(this);
     this.state = {
       email: null,
-      password: null,
       authenticated: false,
     };
   }
@@ -29,21 +26,14 @@ class App extends React.Component {
     }
   }
 
-  changeEmail(e) {
-    this.setState({email: e.target.value});
-  }
-  changePassword(e) {
-    this.setState({password: e.target.value});
-  }
-
-  loginAjax() {
-    console.log('sending', this.state.email);
+  loginAjax(email, password) {
+    console.log(email);
     $.ajax({
       url: '/login',
       method: 'POST',
       contentType: 'application/json',
       // data: JSON.stringify({email: this.state.email, password: this.state.password}),
-      data: JSON.stringify({email: this.state.email, password: this.state.password}),
+      data: JSON.stringify({email: email, password: password}),
       success: (data) => { console.log(data); }
     });
   }
@@ -52,7 +42,7 @@ class App extends React.Component {
     return (
         <Router history={hashHistory}>
           <Route path='/' component={NotFound} onEnter={this.requireAuth} />
-          <Route changeEmail={this.changeEmail} changePassword={this.changePassword} loginAjax={this.loginAjax} path='/login' component={Login} />
+          <Route loginAjax={this.loginAjax} path='/login' component={Login} />
           <Route path='*' component={NotFound} />
         </Router>
     );
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 
-const Login = (props) => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={props.route.loginAjax}>
-        <input onChange={props.route.changeEmail} type="text" placeholder="Email" /><br/>
-        <input onChange={props.route.changePassword} type="password" placeholder="Password" /><br/>
-        <button>Login</button>
-      </form>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.state = {
+      email: null,
+      password: null
+    };
+  }
+
+  changeEmail(e) {
+    this.setState({email: e.target.value});
+  }
+  changePassword(e) {
+    this.setState({password: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.props.route.loginAjax.bind(null, this.state.email, this.state.password)}>
+          <input onChange={this.changeEmail} type="text" placeholder="Email" /><br/>
+          <input onChange={this.changePassword} type="password" placeholder="Password" /><br/>
+          <button>Login</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 
 module.exports = Login;
